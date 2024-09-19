f4b9cb4224cb6c96be7a43f0095f085d50fa17bc
ornellaBottiggi <bottiggiornella@gmail.com>
2024-09-19 16:51:46 -0300

    tp 4 tests

---
+++ test/unq/poo2/tp3/bottiggi/Tp4Test.java
@@ -0,0 +1,19 @@
+package unq.poo2.tp3.bottiggi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Tp4Test {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
